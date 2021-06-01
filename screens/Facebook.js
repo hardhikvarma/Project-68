0f4204c4026d8523b203112e70147b5779a2a1b2
hardhikvarma <74197587+hardhikvarma@users.noreply.github.com>
2021-06-01 17:13:19 +0530

    Add files via upload

---
+++ screens/Facebook.js
@@ -0,0 +1,11 @@
+import React from 'react';
+ import { Text, View } from 'react-native';
+ import AppHeader from '../components/AppHeader'
+ 
+ export default class Facebook extends React.Component { 
+render() { 
+    return ( 
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}> 
+    <AppHeader/>
+    <Text>Facebook</Text> 
+    </View> ); } }
